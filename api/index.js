a233d0ef7ef53b6c2aed74e0686a26c471aef843
Jindal <jindal1632004@gmail.com>
2024-07-22 19:57:35 +0530

    create middleware and function to handle errors

--- api/index.js
+++ api/index.js
@@ -23,3 +23,13 @@ app.listen(3000, ()=> console.log("server started!!"))
 
 app.use('/api/user', userRoute);
 app.use('/api/auth', authRoute);
+
+app.use((err, req, res, next )=>{
+    const statusCode = err.status || 500;
+    const message = err.message || 'Something went wrong!';
+    res.status(statusCode).json({
+        success: false,
+        statusCode,
+        message
+    })
+})
