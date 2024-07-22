b680d96285c53ddd303e3a8c07ec6f9eeff29ae0
Jindal <jindal1632004@gmail.com>
2024-07-22 19:32:24 +0530

    signup api route

--- api/index.js
+++ api/index.js
@@ -2,6 +2,8 @@ import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRoute from './router/user.route.js'
+import authRoute from './router/auth.route.js'
+
 
 dotenv.config()
 let uri = process.env.MONGO
@@ -15,7 +17,9 @@ mongoose.connect(uri)
     console.log('Error connecting to database',err);    
 })
 const app = express();
+app.use(express.json());
 
 app.listen(3000, ()=> console.log("server started!!"))
 
-app.use('/api/user', userRoute)
+app.use('/api/user', userRoute);
+app.use('/api/auth', authRoute);
