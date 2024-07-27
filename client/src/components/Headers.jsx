24acbc67c95cdde63879b6b7cd29e3b39dc4527c
Jindal <jindal1632004@gmail.com>
2024-07-27 18:51:39 +0530

    complete dark mode functionality

--- client/src/components/Headers.jsx
+++ client/src/components/Headers.jsx
@@ -2,12 +2,15 @@ import { Button, Navbar, NavbarToggle, TextInput, Dropdown, Avatar } from 'flowb
 import React from 'react'
 import { Link, useLocation } from 'react-router-dom'
 import {AiOutlineSearch} from 'react-icons/ai'
-import {FaMoon} from 'react-icons/fa'
-import { useSelector } from 'react-redux'
+import {FaMoon, FaSun} from 'react-icons/fa'
+import { useSelector, useDispatch } from 'react-redux'
+import {toggleTheme} from '../redux/theme/themeSlice'
 
 export default function Headers() {
     const path= useLocation().pathname
+    const dispatch = useDispatch()
     const {currentUser} = useSelector(state => state.user)
+    const {theme} = useSelector(state => state.theme)
     return (
         <Navbar className='border-b-2'>
             <Link to='/' className='self-center whitespace-nowrap 
@@ -28,8 +31,9 @@ export default function Headers() {
             <AiOutlineSearch/>
             </Button>
             <div className='flex gap-2 md:order-2'>
-                <Button className='w-12 h-10 hidden sm:inline' color='gray' pill>
-                    <FaMoon/>
+                <Button className='w-12 h-10 hidden sm:inline' color='gray' pill 
+                onClick={()=> dispatch(toggleTheme())}>
+                {theme === 'light' ? <FaSun/> : <FaMoon/>} 
                 </Button>
                 {currentUser ? (
           <Dropdown
@@ -37,7 +41,8 @@ export default function Headers() {
             inline
             label={
               <Avatar alt='user' img={currentUser.profilePicture} rounded />
-            }> <Dropdown.Header>
+            }> 
+            <Dropdown.Header>
               <span className='block text-sm'>@{currentUser.username}</span>
               <span className='block text-sm font-medium truncate'>
                 {currentUser.email}
